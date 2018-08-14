073f01f90e6ffac35bceaaa70f4a33e1e6f9d3fc
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-08-14 15:00:46 -0600

    added highlighted field to js form validation

--- scripts.js
+++ scripts.js
@@ -2,14 +2,17 @@ function validate() {
       var n = document.forms["bw-form"]["first_name"].value;
       var l = document.forms["bw-form"]["last_name"].value;
       var e = document.forms["bw-form"]["email"].value;
-      if (n == "") {
+        if (n == "") {
           alert("Name must be filled out");
+          document.getElementById('fname').style.borderColor = "red";
           return false;
         } else if (l == "") {
         	alert("Last name must be filled out");
+          document.getElementById('lname').style.borderColor = "red";
           return false;
         } else if (e == "") {
         	alert("Email must be filled out");
+          document.getElementById('email').style.borderColor = "red";
           return false;
         }
   }
